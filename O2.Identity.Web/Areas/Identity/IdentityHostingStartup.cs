46e3216688660aafd822543242eb8d0223a22a2d
Denis P <live-dev@hotmail.com>
2020-09-19 14:48:17 +0300

    added Views

---
+++ O2.Identity.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using O2.Identity.Web.Data;
+
+[assembly: HostingStartup(typeof(O2.Identity.Web.Areas.Identity.IdentityHostingStartup))]
+namespace O2.Identity.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
