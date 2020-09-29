b0280f969d6c3ac12fb7be63f4b2bf97af05da04
Denis P <live-dev@hotmail.com>
2020-09-29 14:22:26 +0300

    last

--- O2.Identity.Web/Startup.cs
+++ O2.Identity.Web/Startup.cs
@@ -26,8 +26,11 @@ namespace TokenServiceApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration["ConnectionString"];
+            Console.WriteLine($"ConnectionString = {connectionString}");
+
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(connectionString));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
