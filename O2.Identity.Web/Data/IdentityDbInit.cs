3529b808d76740b017103ee0c84d375197c2ba6e
Denis P <live-dev@hotmail.com>
2020-10-05 16:49:24 +0300

    updated file initdb

--- O2.Identity.Web/Data/IdentityDbInit.cs
+++ O2.Identity.Web/Data/IdentityDbInit.cs
@@ -16,7 +16,8 @@ namespace O2.Identity.Web.Data
             UserManager<O2User> userManager)
         {
             //create database schema if none exists
-            context.Database.EnsureDeleted();
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreatedAsync();
             context.Database.Migrate();
             //If there is already an Administrator role, abort
             //  if (context.Roles.Any(r => r.Name == "Administrator")) return;
