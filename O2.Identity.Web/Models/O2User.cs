ae83fc2c0a35ca7eb5a3970692944804370f0000
Denis P <live-dev@hotmail.com>
2020-11-30 01:15:36 +0300

    add ext fields -completed

--- O2.Identity.Web/Models/O2User.cs
+++ O2.Identity.Web/Models/O2User.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace O2.Identity.Web.Models
@@ -15,5 +16,8 @@ namespace O2.Identity.Web.Models
         
         public string Lastname { get; set; }
         public string Firstname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
