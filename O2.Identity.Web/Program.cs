0b443562454c718031f7fbf27876da6e05abf202
Denis P <live-dev@hotmail.com>
2020-11-29 22:00:57 +0300

    updated program.cs

--- O2.Identity.Web/Program.cs
+++ O2.Identity.Web/Program.cs
@@ -15,46 +15,56 @@ namespace O2.Identity.Web
     {
         public static void Main(string[] args)
         {
-           
+            try
+            {
+                var host = BuildWebHost(args);
 
-            var host = BuildWebHost(args);
-
-            Log.Logger = new LoggerConfiguration()
+                Log.Logger = new LoggerConfiguration()
                             .Enrich.FromLogContext()
                             .WriteTo.Console()
                             .CreateLogger();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                try
-                {
-                    Log.Information("O2.Identity.Web - Starting up");
-
-                var services = scope.ServiceProvider;
-                    //var logger = services.GetRequiredService<ILogger<Program>>();
-              
-                    Log.Information("========== Starting Application ==========");
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-
-                    var userManager = services.GetRequiredService<UserManager<O2User>>();
-                    // var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-                    IdentityDbInit.Initialize(context, userManager);
-                }
-                catch (Exception ex)
+                using (var scope = host.Services.CreateScope())
                 {
                     
-                    Log.Error(ex, "An error occurred while seeding the AuthorizationServer database.");
-                }
-            }
+                        Log.Information("O2.Identity.Web - Starting up");
 
-            host.Run();
+                        var services = scope.ServiceProvider;
+                        //var logger = services.GetRequiredService<ILogger<Program>>();
+                
+                        Log.Information("========== Starting Application ==========");
+                        var context = services.GetRequiredService<ApplicationDbContext>();
+
+                        var userManager = services.GetRequiredService<UserManager<O2User>>();
+                        // var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                        IdentityDbInit.Initialize(context, userManager);
+                    
+                }
+                host.Run();
+            
+            }
+            catch (Exception ex)
+            {
+                
+                Log.Error(ex, "An error occurred while seeding the AuthorizationServer database.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseSerilog() // <- Add this line
+                // Add the following lines
+                // .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                // .ReadFrom.Configuration(hostingContext.Configuration))
+                // .UseSerilog((context, config) =>
+                // {
+                //     config.ReadFrom.Configuration(context.Configuration);
+                // })
                 .Build();
     }
 }
