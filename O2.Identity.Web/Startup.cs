876f3ad8405868d47e6b98563504264bafadb120
Denis P <live-dev@hotmail.com>
2020-12-09 23:45:14 +0300

    add new fix before rebase
    https://o2bionics.atlassian.net/browse/O2P-12

--- O2.Identity.Web/Startup.cs
+++ O2.Identity.Web/Startup.cs
@@ -45,7 +45,7 @@ namespace O2.Identity.Web
 
         public IConfiguration Configuration { get; }
 
-        
+        public bool IsProduction { get; set; }
         
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -200,7 +200,6 @@ namespace O2.Identity.Web
 
         }
 
-        public bool IsProduction { get; set; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -276,7 +275,7 @@ namespace O2.Identity.Web
             });
         }
 
-        public bool IsProduction { get; set; }
+ 
     }
 
 }
