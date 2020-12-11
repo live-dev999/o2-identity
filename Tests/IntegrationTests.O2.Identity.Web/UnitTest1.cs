6bb733a9af1eb2fdafa1f9a4b4b8ccfc9de39e35
Denis P <live-dev@hotmail.com>
2020-12-11 19:23:38 +0300

    added test projects

---
+++ Tests/IntegrationTests.O2.Identity.Web/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests.O2.Identity.Web
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
