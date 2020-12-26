26e5ffcd5cc0954754c2117fae3651c4b4ab5072
Denis P <live-dev@hotmail.com>
2020-12-26 05:23:40 +0300

    added referral

--- O2.Identity.Web/Controllers/UserController.cs
+++ O2.Identity.Web/Controllers/UserController.cs
@@ -23,5 +23,14 @@ namespace O2.Identity.Web.Controllers
             var users = await _userManager.Users.Where(x => x.IsSpecialist).ToListAsync();
             return Ok(users);
         }
+        
+        [HttpGet("referrals/{userId}")]
+        public async Task<IActionResult>  GetReferrals(string userId)
+        {
+            //string userId
+             var users = await _userManager.Users.Where(x => x.SpecialistId==userId).ToListAsync();
+            //var users = await _userManager.Users.Where(x => x.IsSpecialist).ToListAsync();
+            return Ok(users);
+        }
     }
 }
