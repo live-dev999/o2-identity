7a5c01deeaab9fb42edd0d84f5b48c50fd3f005c
Denis P <live-dev@hotmail.com>
2021-01-26 19:13:27 +0300

    updated helm packege

--- O2.Identity.Web/Controllers/AccountController.cs
+++ O2.Identity.Web/Controllers/AccountController.cs
@@ -285,13 +285,27 @@ namespace O2.Identity.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
         {
-            TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
+            // string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            // string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+            //
+            // TwilioClient.Init(accountSid, authToken);
+            //
+            // var message = MessageResource.Create(
+            //     body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+            //     from: new Twilio.Types.PhoneNumber("+15017122661"),
+            //     to: new Twilio.Types.PhoneNumber("+15558675310")
+            // );
 
-            var message = await MessageResource.CreateAsync(
-                body: "O2 Account: ",
-                @from: new Twilio.Types.PhoneNumber(_verification.Config.PhoneNumber),
-                to: new Twilio.Types.PhoneNumber("+375447987208")
-            );
+            // // Console.WriteLine(message.Sid);
+            // TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
+            //
+            // var message = await MessageResource.CreateAsync(
+            //     body: "O2 Platfrom",
+            //     @from: new Twilio.Types.PhoneNumber(_verification.Config.PhoneNumber),
+            //     to: new Twilio.Types.PhoneNumber("+375447987208")
+            // );
+            
+            
             
             // if(model.SMSCode)
             ViewData["ReturnUrl"] = returnUrl;
@@ -354,6 +368,18 @@ namespace O2.Identity.Web.Controllers
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
+                    
+                    // Find your Account Sid and Token at twilio.com/console
+                    TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
+
+                    var message = MessageResource.Create(
+                        body: $"\"#PF_R COMMUNITY\". https://pfr-centr.com. Username: {model.Email}, Password: {model.Password}.",
+                        from: new Twilio.Types.PhoneNumber(_verification.Config.PhoneNumber),
+                        to: new Twilio.Types.PhoneNumber(model.PhoneNumber)
+                    );
+                    
+                    _logger.LogInformation($"Send sms to account PhoneNumber = {message.To} ,SID SMS= {message.Sid} ");
+                    
                     return RedirectToLocal(returnUrl);
                 }
                 AddErrors(result);
