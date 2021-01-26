8872797aa5592e6368c90bdb17c5a36a0d4b5640
Denis P <live-dev@hotmail.com>
2021-01-26 23:09:20 +0300

    updated controller for send sms

--- O2.Identity.Web/Controllers/SmsController.cs
+++ O2.Identity.Web/Controllers/SmsController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using O2.Identity.Web.Services;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
@@ -10,22 +11,34 @@ namespace O2.Identity.Web.Controllers
     public class SmsController: Controller
     {
         private readonly IVerification _verification;
+        private readonly ILogger<SmsController> _logger;
 
-        public SmsController(IVerification verification)
+        public SmsController(IVerification verification, ILogger<SmsController> logger)
         {
             _verification = verification;
+            _logger = logger;
         }
-        [HttpGet("send")]
-        public async Task<IActionResult> Send()
+        
+        [HttpPost("send")]
+        public async Task<IActionResult> Send(string username, string password,string phoneNumber)
         {
             // Find your Account Sid and Token at twilio.com/console
-          TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
+            // if (_verification.Config.NotificationSms)
+            // {
+                // Find your Account Sid and Token at twilio.com/console
+                TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
 
-            var message = MessageResource.Create(
-                body: "I love you Veronica!.",
-                from: new Twilio.Types.PhoneNumber(_verification.Config.PhoneNumber),
-                to: new Twilio.Types.PhoneNumber("+375333762741")
-            );
+                var message = MessageResource.Create(
+                    body:
+                    $"\"#PF_R COMMUNITY\". https://pfr-centr.com. Username: {username}, Password: {password}.",
+                    from: new Twilio.Types.PhoneNumber(_verification.Config.PhoneNumber),
+                    to: new Twilio.Types.PhoneNumber(phoneNumber)
+                );
+
+
+                _logger.LogInformation(
+                    $"Send sms to account PhoneNumber = {message.To} ,SID SMS= {message.Sid} ");
+            // }
 
             // Console.WriteLine(message.Sid);
             // TwilioClient.Init(_verification.Config.AccountSid, _verification.Config.AuthToken);
